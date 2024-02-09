c7d557a90d2fb14019be8ad58aa77fdfa63771cc
Nikolay Sidorenko <gsidnick@gmail.com>
2024-02-09 15:12:20 +0300

    feat: implement server main callback

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import http from 'node:http';
 import dotenv from 'dotenv';
 import { DEFAULT_HOST, DEFAULT_PORT } from './constants';
+import { Params } from './types';
+import router from './router';
 
 dotenv.config();
 
@@ -8,7 +10,28 @@ const HOST: string = String(process.env.HOST) || DEFAULT_HOST;
 const PORT: number = Number(process.env.PORT) || DEFAULT_PORT;
 
 const server = http.createServer((req, res): void => {
-  res.end();
+  try {
+    let route = `${req.method} ${req.url}`;
+    const params: Params = {};
+    const uuid = route.match(/\/api\/users\/(.*)/);
+
+    if (uuid) {
+      const id = uuid[1];
+      route = route.replace(id, ':id');
+      params.id = id;
+    }
+
+    const handler = router.get(route);
+    if (handler) {
+      handler({ req, res, params });
+    } else {
+      res.statusCode = 404;
+      res.end('404 Not Found');
+    }
+  } catch {
+    res.statusCode = 500;
+    res.end('500 Internal Server Error');
+  }
 });
 
 server.listen(PORT, HOST, () => {
