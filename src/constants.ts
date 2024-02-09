aeec21a3e5e599e0cd5e0e7d68cb3696d95ed243
Nikolay Sidorenko <gsidnick@gmail.com>
2024-02-09 16:57:03 +0300

    feat: implement getUser method

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const DEFAULT_HOST = 'localhost';
 export const DEFAULT_PORT = 5000;
+export const UUID_PATTERN = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
--- src/controller.ts
+++ src/controller.ts
@@ -1,4 +1,5 @@
 import { Users, ControllerProps } from './types';
+import { UUID_PATTERN } from './constants';
 import db from './db';
 
 const getUsers = ({ res }: ControllerProps): void => {
@@ -9,6 +10,33 @@ const getUsers = ({ res }: ControllerProps): void => {
   res.end(JSON.stringify(users));
 };
 
+const getUser = ({ res, params }: ControllerProps): void => {
+  let id: string = '';
+  if (params) id = params.id;
+
+  if (id && !UUID_PATTERN.test(id)) {
+    res.statusCode = 400;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.end('400 Bad Request');
+    return;
+  }
+
+  const records = db.filter((item) => item.id === id);
+
+  if (records.length === 0) {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.end('404 Not Found');
+    return;
+  }
+
+  const [user] = records;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  res.end(JSON.stringify(user));
+};
+
 export default {
   getUsers,
+  getUser,
 };
