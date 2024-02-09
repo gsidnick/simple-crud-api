ca056bf8595897c6d04dc289d570d809f3685ed7
Nikolay Sidorenko <gsidnick@gmail.com>
2024-02-09 21:10:33 +0300

    feat: implement multi-mode

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const DEFAULT_HOST = 'localhost';
 export const DEFAULT_PORT = 5000;
+export const DEFAULT_MODE = false;
+export const HOST: string = String(process.env.HOST) || DEFAULT_HOST;
+export const PORT: number = Number(process.env.PORT) || DEFAULT_PORT;
+export const MULTI_MODE: boolean = Boolean(process.env.MULTI_MODE) || DEFAULT_MODE;
 export const UUID_PATTERN = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
